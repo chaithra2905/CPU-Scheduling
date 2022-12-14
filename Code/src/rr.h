08b3ff7968cf5cad69d9b10b90e3d520e014467b
chaithra2905 <60225390+chaithra2905@users.noreply.github.com>
2022-12-15 00:41:00 +0530

    Add files via upload

---
+++ Code/src/rr.h
@@ -0,0 +1,14 @@
+struct Node
+ {
+ 	int pid;
+ 	int arri;
+ 	int bur;
+ 	struct Node *next;
+ }*start=NULL,*tail=NULL,*start1=NULL,*tail1=NULL;
+ struct Node1
+{
+	int  init;
+	int data;
+	int com;
+	struct Node1 *next;
+}*head=NULL,*tail2=NULL;
